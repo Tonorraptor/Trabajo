c553ecf03989bfd50b850e840926fa7fd9009a3a
Tonorraptor <121475061+Tonorraptor@users.noreply.github.com>
2023-06-24 11:07:26 -0500

    Julio
    Envio

---
+++ Assets/Script/Movement.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody rb;
+    public GameObject enemy;
+    public float time;
+    public float maxTime;
+
+    public GameObject iPoint;
+    public GameObject bullet;
+    public float buelletSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+        EnemyMechanic();
+        ShoterController();
+    }
+    public void PlayerMovement()
+    {
+        if (Input.GetKey("d"))
+        {
+            rb.velocity = transform.right * speed;
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.velocity = -transform.right * speed;
+        }
+    }
+    public void EnemyMechanic()
+    {
+        if (time <= 0)
+        {
+            time = maxTime;
+            int positionEnemy = Random.Range(-13, 6);
+            GameObject timeEnemy = Instantiate(enemy, new Vector3(positionEnemy, 18, 0), Quaternion.identity);
+            Destroy(timeEnemy, 4);
+        }
+        time -= Time.deltaTime;
+    }
+    public void ShoterController()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject timeBullet = Instantiate(bullet, iPoint.transform.position, iPoint.transform.rotation);
+            Rigidbody rgBullet = timeBullet.GetComponent<Rigidbody>();
+            rgBullet.AddForce(transform.up * buelletSpeed);
+            Destroy(timeBullet, 5.0F);
+        }
+    }
+}
---
+++ Assets/Script/ScoreText.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class ScoreText : MonoBehaviour
+{
+    private int score;
+    public float time;
+    public float maxtime;
+    private TextMeshProUGUI scoreTextMesh;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreTextMesh = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (maxtime <= time)
+        {
+            score += 1;
+            time = 0;
+
+        }
+        scoreTextMesh.text = "Score: " + score;
+        Condition();
+    }
+    public void Condition()
+    {
+        if (score == 100)
+        {
+            SceneManager.LoadScene("Victoria");
+        }
+    }
+}
